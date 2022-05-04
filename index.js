98dabc5e20b0430d5afdfc233236e535ce81a874
Ado Kukic <kukicadnan@gmail.com>
2022-05-03 17:28:10 -0700

    complete

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const express = require("express");
+
+// Connect MongoDB
+
 let db;
 
 let client = new MongoClient(process.env.MONGODB_URI, {
@@ -13,22 +16,47 @@ client.connect().then((client) => {
 });
 
 const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
   res.json(await db.collection("characters").find().toArray());
 });
 
-app.get("/:id", async (req, res) => {
-  console.log(req.params.id);
+app.get("/winner", async (req, res) => {
   try {
-    const character = await db
+    console.log(req.query);
+    const data = await db
       .collection("characters")
-      .findOne({ _id: Number(req.params.id) });
-    console.log(character);
-    res.json(character);
+      .updateOne({ _id: Number(req.query.id) }, { $inc: { wins: 1 } });
+    res.json(data);
   } catch (error) {
     res.json(error);
   }
 });
 
+app.get("/battle", async (req, res) => {
+  try {
+    const data = await db.collection("characters").find().toArray();
+
+    const one = getCharacter(1, data.length);
+    const two = getCharacter(1, data.length);
+
+    res.render("battle", { one: data[one], two: data[two] });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const character = await db
+    .collection("characters")
+    .findOne({ _id: Number(req.params.id) });
+  res.render("character", { character });
+});
+
 app.listen(8080);
+
+function getCharacter(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
